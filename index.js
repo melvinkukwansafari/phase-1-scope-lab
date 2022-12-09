39a5d919421b8ade625d44429f7ecd7fc2b23a8b
melvinkukwansafari <melvin.kukwan@student.moringaschool.com>
2022-12-09 12:06:05 +0300

    still to pass the last test

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+
+
+function upperCaseCustomerName() {
+    return customerName = customerName.toUpperCase();
+    
+}
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overWriteBestCustomer(melvin) {
+    bestCustomer = 'melvin';
+}
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+
+function customerName() {
+    console.log(customerName);
+}
+customerName();
